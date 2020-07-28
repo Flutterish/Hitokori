4509cee15b14e2fb5b520bbf042a918adbe04040
Flutterish <CharmindYT@gmail.com>
2020-07-28 09:43:49 +0200

    update gitingore

--- obj/Debug/netstandard2.1/osu.Game.Rulesets.Hitokori.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/osu.Game.Rulesets.Hitokori.AssemblyInfo.cs
@@ -11,9 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ppy Pty Ltd")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("osu.Game.Rulesets.Hitokori")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright (c) 2020 ppy Pty Ltd")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("osu.Game.Rulesets.Hitokori")]
