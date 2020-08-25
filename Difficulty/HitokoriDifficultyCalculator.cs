334e3dcabc0f787db50617d33146a56e68c1e337
wixoaGit <wixoag@gmail.com>
2020-08-24 20:48:35 -0400

    Implement reading skill and difficulty calculation

--- Difficulty/HitokoriDifficultyCalculator.cs
+++ Difficulty/HitokoriDifficultyCalculator.cs
@@ -1,29 +1,36 @@
-﻿using osu.Game.Beatmaps;
+﻿using osu.Framework.Input.Events;
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Difficulty;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Difficulty.Skills;
+using osu.Game.Rulesets.Hitokori.Difficulty.Skills;
 using osu.Game.Rulesets.Mods;
 using System;
 using System.Collections.Generic;
 
 namespace osu.Game.Rulesets.Hitokori.Difficulty {
 	public class HitokoriDifficultyCalculator : DifficultyCalculator {
+		private const double star_scaling_factor = 0.04125; //Taken from Taiko's difficulty calculations
+
 		public HitokoriDifficultyCalculator ( Ruleset ruleset, WorkingBeatmap beatmap ) : base( ruleset, beatmap ) { }
 
 		protected override DifficultyAttributes CreateDifficultyAttributes ( IBeatmap beatmap, Mod[] mods, Skill[] skills, double clockRate ) {
+			double readingDifficulty = skills[0].DifficultyValue();
+
 			return new DifficultyAttributes {
-				StarRating = beatmap.BeatmapInfo.StarDifficulty,
+				StarRating = readingDifficulty * star_scaling_factor,
 				Mods = mods,
 				Skills = skills
 			};
 		}
 
 		protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects ( IBeatmap beatmap, double clockRate ) {
-			return Array.Empty<DifficultyHitObject>();
+			for (int i = 1; i < beatmap.HitObjects.Count; i++)
+				yield return new HitokoriDifficultyHitObject(beatmap.HitObjects[i], beatmap.HitObjects[i - 1], clockRate);
 		}
 
 		protected override Skill[] CreateSkills ( IBeatmap beatmap ) {
-			return Array.Empty<Skill>(); // TODO actually do the DifficultyCalculator
+			return new Skill[] { new Reading() };
 		}
 	}
 }
--- Difficulty/HitokoriDifficultyHitObject.cs
+++ Difficulty/HitokoriDifficultyHitObject.cs
@@ -1,10 +1,31 @@
 ﻿using osu.Game.Rulesets.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Hitokori.Objects;
+using osu.Game.Rulesets.Hitokori.Objects.Base;
 using osu.Game.Rulesets.Objects;
 
 namespace osu.Game.Rulesets.Hitokori.Difficulty {
 	public class HitokoriDifficultyHitObject : DifficultyHitObject {
-		public HitokoriDifficultyHitObject ( HitObject hitObject, HitObject lastObject, double clockRate ) : base( hitObject, lastObject, clockRate ) {
+		public bool ChangedDirection { get; private set; }
 
+		/// <summary>
+		/// The angle traveled to hit this object
+		/// </summary>
+		public double HitAngle { get; private set; }
+
+		/// <summary>
+		/// The angle that this object is held, if applicable
+		/// </summary>
+		public double? HoldAngle { get; private set; }
+
+		public HitokoriDifficultyHitObject ( HitObject hitObject, HitObject lastObject, double clockRate ) : base( hitObject, lastObject, clockRate ) {
+			HitokoriTileObject hitokoriObject = (HitokoriTileObject)hitObject;
+			HitokoriTileObject lastHitokoriObject = (HitokoriTileObject)lastObject;
+
+			ChangedDirection = hitokoriObject.FirstPoint.ChangedDirection;
+			HitAngle = lastHitokoriObject.LastPoint.AngleOffset;
+			if (hitokoriObject is HoldTile) {
+				HoldAngle = ((HoldTile)hitokoriObject).FirstPoint.AngleOffset;
+            }
 		}
 	}
 }
