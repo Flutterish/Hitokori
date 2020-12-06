68a90fee01ad8174eac1a63be0d14b599c11e153
Flutterish <CharmindYT@gmail.com>
2020-12-06 17:24:25 +0100

    "you need to work with method params"

--- Hitokori/Objects/Drawables/Tiles/DrawableTilePoint.cs
+++ Hitokori/Objects/Drawables/Tiles/DrawableTilePoint.cs
@@ -65,7 +65,7 @@ namespace osu.Game.Rulesets.Hitokori.Objects.Drawables.Tiles {
 
 		protected override void CheckForResult ( bool userTriggered, double timeOffset ) {
 			// to make sure a result is set
-			if ( !TilePoint.CanBeHitAfter( TilePoint.TimeAtOffset( timeOffset ) ) || ( TilePoint.Next?.HitTime - TilePoint.Duration / 2 < Clock.CurrentTime == true ) /*( timeOffset > 0 && TilePoint.Next?.HitWindows.ResultFor( TilePoint.Next.TimeOffsetAt( TilePoint.TimeAtOffset( timeOffset ) ) ) != HitResult.None)*/ ) {
+			if ( !TilePoint.CanBeHitAfter( TilePoint.TimeAtOffset( timeOffset ) ) || timeOffset > TilePoint.Duration / 2 ) {
 				SetResult( HitResult.Miss ); // NOTE when rewinding this sets off on first tile, at an offset from its actual hit time
 			}
 		}
